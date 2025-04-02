3622890acba10627bd7e8b2fd625cb0e5c261f43
KathrynOliveira <k.olliver18@gmail.com>
2025-04-02 19:54:45 -0300

    aula 23 - criando component menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import "./styles/global.css";
 import { Container } from "./components/Container";
-import { Heading } from "./components/Heading";
 import { Logo } from "./components/Logo";
+import { Menu } from "./components/Menu";
 
 export function App() {
   return (
+    <>
       <Container>
-          <Logo />
+        <Logo />
       </Container>
+
+      <Container>
+        <Menu />
+      </Container>
+    </>
   );
 }
