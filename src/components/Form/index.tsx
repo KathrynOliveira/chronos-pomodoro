0cb1abf71b37ed5f66bb165da507b15a870cc18f
KathrynOliveira <k.olliver18@gmail.com>
2025-04-06 21:11:18 -0300

    aula 52, 53 e 54 - validando form e criando utils

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -4,11 +4,51 @@ import {Button} from '../Button';
 import styles from './styles.module.css';
 import { PlayCircleIcon } from 'lucide-react';
 import { useState } from 'react';
+import { TaskModel } from '../../models/TaskModel';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import { getNextCycle } from '../../utils/getNextCycle';
 
 export function Form() {
+    const { state, setState } = useTaskContext();
     const [taskName, setTaskName] = useState('');
+
+    const nextCycle = getNextCycle(state.currentCycle)
+
     function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) { 
         event.preventDefault();
+
+        if (taskName === null) return;
+
+        const task = taskName.trim();
+        if (!task) {
+            alert('Por favor, insira um nome para a tarefa.');
+            return;
+        }
+
+        const newTask: TaskModel = {
+            id: Date.now().toString(),
+            name: task,
+            startDate: Date.now(),
+            completeDate: null,
+            interruptDate: null,
+            duration: 1,
+            type: "workTime",
+        }
+
+        const secondsRemaining = newTask.duration * 60
+
+        setState(prevState => {
+            return {
+                ...prevState,
+                activeTask: newTask,
+                currentCycle: nextCycle,
+                secondsRemaining,
+                formattedSecondsRemaining: '00:00',
+                task: [...prevState.tasks, newTask],
+                config: {...prevState.config},
+            }
+        })
+
     }
     return(
         <div>
