3feeb83e43c5322b05d079d7e992a11532d05464
Kathryn Oliveira <k.olliver18@gmail.com>
2025-04-25 13:28:56 -0300

    aula 74 tocando audio

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,8 +3,8 @@ import styles from './styles.module.css';
 export function Footer(){
     return (
         <footer className={styles.footer}>
-            <a href="">Entenda como funciona a técnica pomodoro</a>
-            <a href="">Chronos Pomodoro &copy; {new Date().getFullYear() } - Feito com s2</a>
+            <a href="">Entenda como funciona a técnica pomodoro 🍅</a>
+            <a href="">Chronos Pomodoro &copy; {new Date().getFullYear() } - Feito com 💚</a>
         </footer>
     )
 }
